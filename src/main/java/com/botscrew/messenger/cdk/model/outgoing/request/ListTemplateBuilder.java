3819802ff70f465d54fe0d654e9c313d0158f47d
Oles Pankiv <oles.pankiv@botscrew.com>
2018-03-13 14:14:15 +0200

    Added Request builders in outgoing.request package

---
+++ src/main/java/com/botscrew/messenger/cdk/model/outgoing/request/ListTemplateBuilder.java
@@ -0,0 +1,51 @@
+package com.botscrew.messenger.cdk.model.outgoing.request;
+
+import com.botscrew.messenger.cdk.model.outgoing.button.Button;
+import com.botscrew.messenger.cdk.model.outgoing.template.TemplateAttachment;
+import com.botscrew.messenger.cdk.model.outgoing.template.TemplateElement;
+import com.botscrew.messenger.cdk.model.outgoing.template.list.ListTemplateMessage;
+import com.botscrew.messenger.cdk.model.outgoing.template.list.ListTemplatePayload;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTemplateBuilder extends RequestBuilder<ListTemplateBuilder> {
+    private List<TemplateElement> elements = new ArrayList<>();
+    private List<Button> buttons = new ArrayList<>();
+    private ListTemplatePayload.TopElementStyle topElementStyle;
+
+    public ListTemplateBuilder elements(List<TemplateElement> elements) {
+        this.elements = elements;
+        return this;
+    }
+
+    public ListTemplateBuilder element(TemplateElement element) {
+        elements.add(element);
+        return this;
+    }
+
+    public ListTemplateBuilder buttons(List<Button> buttons) {
+        this.buttons = buttons;
+        return this;
+    }
+
+    public ListTemplateBuilder button(Button button) {
+        buttons.add(button);
+        return this;
+    }
+
+    public ListTemplateBuilder topElementStyle(ListTemplatePayload.TopElementStyle style) {
+        this.topElementStyle = style;
+        return this;
+    }
+
+    @Override
+    public Request build() {
+        Request request = super.build();
+        request.setMessage(new ListTemplateMessage(
+                new TemplateAttachment(
+                        new ListTemplatePayload(elements, buttons, topElementStyle))));
+        return request;
+    }
+
+}
