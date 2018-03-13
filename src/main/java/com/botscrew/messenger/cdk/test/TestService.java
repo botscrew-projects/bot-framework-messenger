4e3bc53228cde4b40889933ffd752b6fcb75eed1
Oles Pankiv <oles.pankiv@botscrew.com>
2018-03-13 14:14:55 +0200

    Updated Test.java class

--- src/main/java/com/botscrew/messenger/cdk/test/TestService.java
+++ src/main/java/com/botscrew/messenger/cdk/test/TestService.java
@@ -3,30 +3,100 @@ package com.botscrew.messenger.cdk.test;
 import com.botscrew.botframework.annotation.ChatEventsProcessor;
 import com.botscrew.botframework.annotation.Text;
 import com.botscrew.messenger.cdk.model.MessengerUser;
-import com.botscrew.messenger.cdk.model.outgoing.GenericElement;
-import com.botscrew.messenger.cdk.model.outgoing.QuickReply;
-import com.botscrew.messenger.cdk.model.outgoing.WebButton;
+import com.botscrew.messenger.cdk.model.incomming.UserInfo;
+import com.botscrew.messenger.cdk.model.outgoing.*;
+import com.botscrew.messenger.cdk.model.outgoing.button.PostbackButton;
+import com.botscrew.messenger.cdk.model.outgoing.button.WebButton;
+import com.botscrew.messenger.cdk.model.outgoing.request.Request;
+import com.botscrew.messenger.cdk.model.outgoing.template.*;
+import com.botscrew.messenger.cdk.model.outgoing.template.generic.GenericTemplateMessage;
+import com.botscrew.messenger.cdk.model.outgoing.template.generic.GenericTemplatePayload;
+import com.botscrew.messenger.cdk.model.outgoing.template.list.ListTemplatePayload;
 import com.botscrew.messenger.cdk.service.Sender;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Collections;
 
+@Slf4j
 @ChatEventsProcessor
 public class TestService {
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @Autowired
     private Sender sender;
 
     @Text
-    public void text(MessengerUser user, String text) {
-        QuickReply postback = QuickReply.postback("Title", "Postback");
-        GenericElement build = GenericElement.builder()
-                .title("Title")
-                .subtitle("subtitle")
+    public void text(MessengerUser user, @Text String text) throws JsonProcessingException {
+        if (text == null || text.equals("a")) {
+            QuickReply postback = QuickReply.postback("Title", "Postback");
+            TemplateElement build = TemplateElement.builder()
+                    .title("Title")
+                    .subtitle("subtitle")
+                    .imageUrl("http://lostfilm-online.ru/uploads/posts/2015-01/1422172418_lost.jpg")
+                    .button(new WebButton("Title", "https://google.com"))
+                    .build();
+
+            TemplateAttachment templateAttachment = new TemplateAttachment(new GenericTemplatePayload(Collections.singletonList(build)));
+            Request request = new Request();
+            request.setMessage(new GenericTemplateMessage(Collections.singletonList(postback), templateAttachment));
+            request.setRecipient(new UserInfo(user.getChatId()));
+            sender.send(request);
+        } else if (text.equals("b")) {
+            sendQuickReplyMessage(user);
+        } else if (text.equals("c")) {
+            sendGenericTemplate(user);
+        } else if (text.equals("d")) {
+            sendListTemplate(user);
+        }
+    }
+
+    public void sendQuickReplyMessage(MessengerUser user) {
+        Request request = Request.quickReply()
+                .text("Some text")
+                .postback("Red", "RED_POSTBACK")
+                .postback("Blue", "BLUE_POSTBACK")
+                .location()
+                .recipient(user)
+                .build();
+        sender.send(request);
+    }
+
+    public void sendGenericTemplate(MessengerUser user) {
+        Request request = Request.genericTemplate()
+                .element(
+                        TemplateElement.builder()
+                                .imageUrl("http://lostfilm-online.ru/uploads/posts/2015-01/1422172418_lost.jpg")
+                                .title("TITLE")
+                                .subtitle("subtitle")
+                                .button(new WebButton("google.com", "https://google.com"))
+                                .build())
+                .recipient(user)
+                .build();
+        sender.send(request);
+    }
+
+    public void sendListTemplate(MessengerUser user) throws JsonProcessingException {
+        TemplateElement listTemplateElement = TemplateElement.builder()
+                .title("TITLE")
+                .subtitle("SUBTITLE")
                 .imageUrl("http://lostfilm-online.ru/uploads/posts/2015-01/1422172418_lost.jpg")
-                .buttons(Collections.singletonList(new WebButton("Title", "https://google.com")))
+                .button(new PostbackButton("Help", "HELP_POSTBACK"))
                 .build();
 
-        sender.send(user, Collections.singletonList(build), Collections.singletonList(postback));
+        Request request = Request.listTemplate()
+                .element(listTemplateElement)
+                .element(listTemplateElement)
+                .element(listTemplateElement)
+                .button(new PostbackButton("Read more", "READ_MORE_POSTBACK"))
+                .topElementStyle(ListTemplatePayload.TopElementStyle.LARGE)
+                .recipient(user)
+                .build();
+
+        sender.send(request);
     }
 }
