01c763964a1868f1b84a8ceb52e4a02fc773f164
VladHanzha <vladyslav.hanzha@gmail.com>
2018-03-23 18:24:04 +0200

    implement MessengerUser from PlatformUser

--- src/main/java/com/botscrew/messengercdk/model/MessengerUser.java
+++ src/main/java/com/botscrew/messengercdk/model/MessengerUser.java
@@ -1,8 +1,15 @@
 package com.botscrew.messengercdk.model;
 
 
-import com.botscrew.botframework.domain.ChatUser;
+import com.botscrew.botframework.domain.user.Chat;
+import com.botscrew.botframework.domain.user.Platform;
+import com.botscrew.botframework.domain.user.PlatformUser;
 
-public interface MessengerUser extends ChatUser {
+public interface MessengerUser extends PlatformUser {
     Long getChatId();
+
+    @Override
+    default Chat getChat() {
+        return () -> Platform.FB_MESSENGER;
+    }
 }
