dcc32e839b0ca57c833a7e7cac45c12b84cfb1a2
VladHanzha <vladyslav.hanzha@gmail.com>
2018-03-27 11:29:13 +0300

    swap page id and user id if message type is echo

--- src/main/java/com/botscrew/messengercdk/service/impl/DefaultReportHandler.java
+++ src/main/java/com/botscrew/messengercdk/service/impl/DefaultReportHandler.java
@@ -57,12 +57,28 @@ public class DefaultReportHandler implements ReportHandler {
         for (Messaging messaging : bundle.getMessaging()) {
             EventType type = typeResolver.resolve(messaging);
 
-            Long pageId = messaging.getRecipient().getId();
+            Long pageId = getPageId(messaging);
+            Long userId = getUserId(messaging);
+
             MessengerBot messengerBot = botProvider.getById(pageId);
             if (messengerBot == null) throw new MessengerCDKException("Bot provider returns NULL for page id: " + pageId);
-            MessengerUser user = userProvider.getByChatIdAndBotId(messaging.getSender().getId(), messengerBot.getId());
+            MessengerUser user = userProvider.getByChatIdAndBotId(userId, messengerBot.getId());
 
             eventHandlers.get(type).handle(user, messaging);
         }
+    }
+
+    private Long getUserId(Messaging messaging) {
+        if (messaging.getMessage() != null && messaging.getMessage().isEcho()) {
+            return messaging.getRecipient().getId();
+        }
+        return messaging.getSender().getId();
+    }
+
+    private Long getPageId(Messaging messaging) {
+        if (messaging.getMessage() != null && messaging.getMessage().isEcho()) {
+            return messaging.getSender().getId();
+        }
+        return messaging.getRecipient().getId();
     }
 }
