4b71ae95cee4ef7e729f107f1abbb0639ef4596c
VladHanzha <vladyslav.hanzha@gmail.com>
2018-03-29 12:36:31 +0300

    Add conditional for handlers

--- src/main/java/com/botscrew/messengercdk/config/EventHandlersConfiguration.java
+++ src/main/java/com/botscrew/messengercdk/config/EventHandlersConfiguration.java
@@ -7,6 +7,7 @@ import com.botscrew.messengercdk.service.impl.handler.BotFrameworkLocationEventH
 import com.botscrew.messengercdk.service.impl.handler.BotFrameworkPostbackEventHandler;
 import com.botscrew.messengercdk.service.impl.handler.BotFrameworkQuickReplyEventHandler;
 import com.botscrew.messengercdk.service.impl.handler.BotFrameworkTextEventHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,21 +15,25 @@ import org.springframework.context.annotation.Configuration;
 public class EventHandlersConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean(BotFrameworkLocationEventHandler.class)
     public BotFrameworkLocationEventHandler botFrameworkLocationEventHandler(LocationContainer locationContainer) {
         return new BotFrameworkLocationEventHandler(locationContainer);
     }
 
     @Bean
+    @ConditionalOnMissingBean(BotFrameworkPostbackEventHandler.class)
     public BotFrameworkPostbackEventHandler botFrameworkPostbackEventHandler(PostbackContainer postbackContainer) {
         return new BotFrameworkPostbackEventHandler(postbackContainer);
     }
 
     @Bean
+    @ConditionalOnMissingBean(BotFrameworkQuickReplyEventHandler.class)
     public BotFrameworkQuickReplyEventHandler botFrameworkQuickReplyEventHandler(PostbackContainer postbackContainer) {
         return new BotFrameworkQuickReplyEventHandler(postbackContainer);
     }
 
     @Bean
+    @ConditionalOnMissingBean(BotFrameworkTextEventHandler.class)
     public BotFrameworkTextEventHandler botFrameworkTextEventHandler(TextContainer textContainer) {
         return new BotFrameworkTextEventHandler(textContainer);
     }
